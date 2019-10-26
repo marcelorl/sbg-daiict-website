bb9aa6b7c5fb9f062ff5cd485ea6bf61bea6a720
marcelorl <marcelo_rl2@hotmail.com>
2019-10-26 18:11:44 -0300

    refac(Header): reactstrap installation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,50 +1,58 @@
-import React from "react";
-import { ButtonToolbar, DropdownButton, MenuItem } from "react-bootstrap";
-import Navbar, { Brand } from "react-bootstrap/lib/Navbar";
+import React from 'react'
+import {
+  Button,
+  Navbar,
+  NavbarBrand,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap'
 
-import { brandInfo, menuItems } from "../../data/header";
-import Sidebar from "../Sidebar";
-import { FaThList } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { brandInfo, menuItems } from '../../data/header'
+import Sidebar from '../Sidebar'
+import { FaThList } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const DropdownButton = props => (
+  <Button {...props} outline color="secondary" className="text-dark" />
+)
 
 function Header() {
   return (
-    <div id="wrapper" className="content">
-      <Navbar fluid style={{ margin: 0 }}>
-        <ButtonToolbar className="dropdown-home" style={{ marginTop: 10 }}>
-          <DropdownButton
-            bsStyle="default"
-            noCaret
-            title={
-              <span>
-                <FaThList />
-              </span>
-            }
-            id="dropdown-size-large"
-          >
+    <div id="wrapper" className="content border-bottom">
+      <Navbar color="light" light className="m-0">
+        <UncontrolledDropdown size="sm" className="dropdown-home mt-1">
+          <DropdownToggle tag={DropdownButton}>
+            <FaThList />
+          </DropdownToggle>
+          <DropdownMenu>
             {menuItems.map(i => (
-              <React.Fragment key={i.to}>
-                <li role="presentation">
-                  <Link to={i.to}>{i.title}</Link>
+              <DropdownItem className="p-0" key={i.to}>
+                <li className="px-3 py-1" role="presentation">
+                  <Link className="text-decoration-none text-dark" to={i.to}>
+                    {i.title}
+                  </Link>
                 </li>
-                <MenuItem divider />
-              </React.Fragment>
+                <DropdownItem divider />
+              </DropdownItem>
             ))}
-          </DropdownButton>
-        </ButtonToolbar>
-        <Brand>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+
+        <NavbarBrand href="/">
           <img
             className="brand-logo"
             src={brandInfo.logo}
             alt={brandInfo.title}
             title={brandInfo.title}
           />
-          <span>{brandInfo.title}</span>
-        </Brand>
+          <span className="text-secondary">{brandInfo.title}</span>
+        </NavbarBrand>
         <Sidebar />
       </Navbar>
     </div>
-  );
+  )
 }
 
-export default Header;
+export default Header
